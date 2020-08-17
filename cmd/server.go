ff45e78bd4b106cf926142760fbee6f5c28a2407
Manuel Wieser <manuel.wieser@allaboutapps.at>
2020-08-17 16:54:30 +0200

    Enable pretty printing of log per default on local dev env.

--- cmd/server.go
+++ cmd/server.go
@@ -38,7 +38,7 @@ func runServer() {
 
 	zerolog.TimeFieldFormat = time.RFC3339Nano
 	zerolog.SetGlobalLevel(config.Logger.Level)
-	if config.Logger.PrettyPrint {
+	if config.Logger.PrettyPrintConsole {
 		log.Logger = log.Output(zerolog.NewConsoleWriter(func(w *zerolog.ConsoleWriter) {
 			w.TimeFormat = "15:04:05"
 		}))
