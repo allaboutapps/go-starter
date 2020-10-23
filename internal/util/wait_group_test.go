a8a7a6b4bd7d43169fd9ec335aec0a6e79bd774a
Mario Ranftl <mario.ranftl@allaboutapps.at>
2020-10-23 12:57:32 +0200

    waitgroup tests better timing

--- internal/util/wait_group_test.go
+++ internal/util/wait_group_test.go
@@ -18,32 +18,32 @@ func TestWaitTimeoutErr(t *testing.T) {
 
 	wg.Add(1)
 	go func() {
-		atomic.AddInt32(&n, 1) // pass
-		time.Sleep(25 * time.Millisecond)
-		atomic.AddInt32(&n, 1) // pass
+		atomic.AddInt32(&n, 1)
+		time.Sleep(10 * time.Millisecond)
+		atomic.AddInt32(&n, 1)
 		wg.Done()
 	}()
 
 	wg.Add(1)
 	go func() {
-		atomic.AddInt32(&n, 1) // pass
+		atomic.AddInt32(&n, 1)
 		wg.Done()
 	}()
 
 	wg.Add(1)
 	go func() {
-		time.Sleep(350 * time.Millisecond)
+		time.Sleep(400 * time.Millisecond)
 		atomic.AddInt32(&n, 1) // available after wg.Wait()
 		wg.Done()
 	}()
 
 	// timeout reached.
-	err := util.WaitTimeout(&wg, 100*time.Millisecond)
+	err := util.WaitTimeout(&wg, 200*time.Millisecond)
 	assert.Equal(t, util.ErrWaitTimeout, err)
 	assert.Equal(t, int32(3), atomic.LoadInt32(&n))
 
 	// ok (after timeout).
-	err = util.WaitTimeout(&wg, 1*time.Second)
+	err = util.WaitTimeout(&wg, 800*time.Second)
 	assert.Equal(t, nil, err)
 	assert.Equal(t, int32(4), atomic.LoadInt32(&n))
 }
