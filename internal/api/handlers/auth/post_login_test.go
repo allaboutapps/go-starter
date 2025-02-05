942918233af80b75c082f5256dacc7a8a634a02f
Eric Klatzer <eric.klatzer@allaboutapps.at>
2025-02-05 09:32:07 +0000

    Simplify the endpoint tests by using test.RequireHTTPError, response snapshots and table-driven tests

--- internal/api/handlers/auth/post_login_test.go
+++ internal/api/handlers/auth/post_login_test.go
@@ -13,6 +13,7 @@ import (
 	"allaboutapps.dev/aw/go-starter/internal/api/middleware"
 	"allaboutapps.dev/aw/go-starter/internal/test"
 	"allaboutapps.dev/aw/go-starter/internal/types"
+	"github.com/labstack/echo/v4"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 	"github.com/volatiletech/null/v8"
@@ -28,7 +29,29 @@ func TestPostLoginSuccess(t *testing.T) {
 		}
 
 		res := test.PerformRequest(t, s, "POST", "/api/v1/auth/login", payload, nil)
+		assert.Equal(t, http.StatusOK, res.Result().StatusCode)
 
+		var response types.PostLoginResponse
+		test.ParseResponseAndValidate(t, res, &response)
+
+		assert.NotEmpty(t, response.AccessToken)
+		assert.NotEqual(t, fixtures.User1AccessToken1.Token, response.AccessToken)
+		assert.NotEmpty(t, response.RefreshToken)
+		assert.NotEqual(t, fixtures.User1RefreshToken1.Token, response.RefreshToken)
+		assert.Equal(t, int64(s.Config.Auth.AccessTokenValidity.Seconds()), *response.ExpiresIn)
+		assert.Equal(t, auth.TokenTypeBearer, *response.TokenType)
+	})
+}
+
+func TestPostLoginSuccessLowercaseTrimWhitespaces(t *testing.T) {
+	test.WithTestServer(t, func(s *api.Server) {
+		fixtures := test.Fixtures()
+		payload := test.GenericPayload{
+			"username": fmt.Sprintf(" %s ", strings.ToUpper(fixtures.User1.Username.String)),
+			"password": test.PlainTestUserPassword,
+		}
+
+		res := test.PerformRequest(t, s, "POST", "/api/v1/auth/login", payload, nil)
 		assert.Equal(t, http.StatusOK, res.Result().StatusCode)
 
 		var response types.PostLoginResponse
@@ -52,18 +75,7 @@ func TestPostLoginInvalidCredentials(t *testing.T) {
 		}
 
 		res := test.PerformRequest(t, s, "POST", "/api/v1/auth/login", payload, nil)
-
-		assert.Equal(t, http.StatusUnauthorized, res.Result().StatusCode)
-
-		var response httperrors.HTTPError
-		test.ParseResponseAndValidate(t, res, &response)
-
-		assert.Equal(t, int64(http.StatusUnauthorized), *response.Code)
-		assert.Equal(t, httperrors.HTTPErrorTypeGeneric, *response.Type)
-		assert.Equal(t, http.StatusText(http.StatusUnauthorized), *response.Title)
-		assert.Empty(t, response.Detail)
-		assert.Nil(t, response.Internal)
-		assert.Nil(t, response.AdditionalData)
+		test.RequireHTTPError(t, res, httperrors.NewFromEcho(echo.ErrUnauthorized))
 	})
 }
 
@@ -75,18 +87,7 @@ func TestPostLoginUnknownUser(t *testing.T) {
 		}
 
 		res := test.PerformRequest(t, s, "POST", "/api/v1/auth/login", payload, nil)
-
-		assert.Equal(t, http.StatusUnauthorized, res.Result().StatusCode)
-
-		var response httperrors.HTTPError
-		test.ParseResponseAndValidate(t, res, &response)
-
-		assert.Equal(t, int64(http.StatusUnauthorized), *response.Code)
-		assert.Equal(t, httperrors.HTTPErrorTypeGeneric, *response.Type)
-		assert.Equal(t, http.StatusText(http.StatusUnauthorized), *response.Title)
-		assert.Empty(t, response.Detail)
-		assert.Nil(t, response.Internal)
-		assert.Nil(t, response.AdditionalData)
+		test.RequireHTTPError(t, res, httperrors.NewFromEcho(echo.ErrUnauthorized))
 	})
 }
 
@@ -99,18 +100,7 @@ func TestPostLoginDeactivatedUser(t *testing.T) {
 		}
 
 		res := test.PerformRequest(t, s, "POST", "/api/v1/auth/login", payload, nil)
-
-		assert.Equal(t, http.StatusForbidden, res.Result().StatusCode)
-
-		var response httperrors.HTTPError
-		test.ParseResponseAndValidate(t, res, &response)
-
-		assert.Equal(t, *middleware.ErrForbiddenUserDeactivated.Code, *response.Code)
-		assert.Equal(t, *middleware.ErrForbiddenUserDeactivated.Type, *response.Type)
-		assert.Equal(t, *middleware.ErrForbiddenUserDeactivated.Title, *response.Title)
-		assert.Empty(t, response.Detail)
-		assert.Nil(t, response.Internal)
-		assert.Nil(t, response.AdditionalData)
+		test.RequireHTTPError(t, res, middleware.ErrForbiddenUserDeactivated)
 	})
 }
 
@@ -122,182 +112,68 @@ func TestPostLoginUserWithoutPassword(t *testing.T) {
 			"password": test.PlainTestUserPassword,
 		}
 
-		fixtures.User2.Password = null.NewString("", false)
+		fixtures.User2.Password = null.String{}
 		rowsAff, err := fixtures.User2.Update(context.Background(), s.DB, boil.Infer())
 		require.NoError(t, err)
 		require.Equal(t, int64(1), rowsAff)
 
 		res := test.PerformRequest(t, s, "POST", "/api/v1/auth/login", payload, nil)
-
-		assert.Equal(t, http.StatusUnauthorized, res.Result().StatusCode)
-
-		var response httperrors.HTTPError
-		test.ParseResponseAndValidate(t, res, &response)
-
-		assert.Equal(t, int64(http.StatusUnauthorized), *response.Code)
-		assert.Equal(t, httperrors.HTTPErrorTypeGeneric, *response.Type)
-		assert.Equal(t, http.StatusText(http.StatusUnauthorized), *response.Title)
-		assert.Empty(t, response.Detail)
-		assert.Nil(t, response.Internal)
-		assert.Nil(t, response.AdditionalData)
+		test.RequireHTTPError(t, res, httperrors.NewFromEcho(echo.ErrUnauthorized))
 	})
 }
 
-func TestPostLoginInvalidUsername(t *testing.T) {
-	test.WithTestServer(t, func(s *api.Server) {
-		payload := test.GenericPayload{
-			"username": "definitely not an email",
-			"password": test.PlainTestUserPassword,
-		}
-
-		res := test.PerformRequest(t, s, "POST", "/api/v1/auth/login", payload, nil)
-
-		assert.Equal(t, http.StatusBadRequest, res.Result().StatusCode)
-
-		var response httperrors.HTTPValidationError
-		test.ParseResponseAndValidate(t, res, &response)
-
-		assert.Equal(t, int64(http.StatusBadRequest), *response.Code)
-		assert.Equal(t, httperrors.HTTPErrorTypeGeneric, *response.Type)
-		assert.Equal(t, http.StatusText(http.StatusBadRequest), *response.Title)
-		assert.Empty(t, response.Detail)
-		assert.Nil(t, response.Internal)
-		assert.Nil(t, response.AdditionalData)
-		assert.NotEmpty(t, response.ValidationErrors)
-		assert.Equal(t, "username", *response.ValidationErrors[0].Key)
-		assert.Equal(t, "body", *response.ValidationErrors[0].In)
-		assert.Equal(t, "username in body must be of type email: \"definitely not an email\"", *response.ValidationErrors[0].Error)
-	})
-}
-
-func TestPostLoginMissingUsername(t *testing.T) {
-	test.WithTestServer(t, func(s *api.Server) {
-		payload := test.GenericPayload{
-			"password": test.PlainTestUserPassword,
-		}
-
-		res := test.PerformRequest(t, s, "POST", "/api/v1/auth/login", payload, nil)
-
-		assert.Equal(t, http.StatusBadRequest, res.Result().StatusCode)
-
-		var response httperrors.HTTPValidationError
-		test.ParseResponseAndValidate(t, res, &response)
-
-		assert.Equal(t, int64(http.StatusBadRequest), *response.Code)
-		assert.Equal(t, httperrors.HTTPErrorTypeGeneric, *response.Type)
-		assert.Equal(t, http.StatusText(http.StatusBadRequest), *response.Title)
-		assert.Empty(t, response.Detail)
-		assert.Nil(t, response.Internal)
-		assert.Nil(t, response.AdditionalData)
-		assert.NotEmpty(t, response.ValidationErrors)
-		assert.Equal(t, "username", *response.ValidationErrors[0].Key)
-		assert.Equal(t, "body", *response.ValidationErrors[0].In)
-		assert.Equal(t, "username in body is required", *response.ValidationErrors[0].Error)
-	})
-}
-
-func TestPostLoginMissingPassword(t *testing.T) {
+func TestPostLoginBadrequest(t *testing.T) {
 	test.WithTestServer(t, func(s *api.Server) {
 		fixtures := test.Fixtures()
-		payload := test.GenericPayload{
-			"username": fixtures.User1.Username,
+
+		tests := []struct {
+			name    string
+			payload test.GenericPayload
+		}{
+			{
+				name: "InvalidUsername",
+				payload: test.GenericPayload{
+					"username": "definitely not an email",
+					"password": test.PlainTestUserPassword,
+				},
+			},
+			{
+				name: "MissingUsername",
+				payload: test.GenericPayload{
+					"password": test.PlainTestUserPassword,
+				},
+			},
+			{
+				name: "MissingPassword",
+				payload: test.GenericPayload{
+					"username": fixtures.User1.Username,
+				},
+			},
+			{
+				name: "EmptyUsername",
+				payload: test.GenericPayload{
+					"username": "",
+				},
+			},
+			{
+				name: "EmptyPassword",
+				payload: test.GenericPayload{
+					"username": fixtures.User1.Username,
+				},
+			},
 		}
 
-		res := test.PerformRequest(t, s, "POST", "/api/v1/auth/login", payload, nil)
+		for _, tt := range tests {
+			t.Run(tt.name, func(t *testing.T) {
+				res := test.PerformRequest(t, s, "POST", "/api/v1/auth/login", tt.payload, nil)
+				assert.Equal(t, http.StatusBadRequest, res.Result().StatusCode)
 
-		assert.Equal(t, http.StatusBadRequest, res.Result().StatusCode)
+				var response httperrors.HTTPValidationError
+				test.ParseResponseAndValidate(t, res, &response)
 
-		var response httperrors.HTTPValidationError
-		test.ParseResponseAndValidate(t, res, &response)
-
-		assert.Equal(t, int64(http.StatusBadRequest), *response.Code)
-		assert.Equal(t, httperrors.HTTPErrorTypeGeneric, *response.Type)
-		assert.Equal(t, http.StatusText(http.StatusBadRequest), *response.Title)
-		assert.Empty(t, response.Detail)
-		assert.Nil(t, response.Internal)
-		assert.Nil(t, response.AdditionalData)
-		assert.NotEmpty(t, response.ValidationErrors)
-		assert.Equal(t, "password", *response.ValidationErrors[0].Key)
-		assert.Equal(t, "body", *response.ValidationErrors[0].In)
-		assert.Equal(t, "password in body is required", *response.ValidationErrors[0].Error)
-	})
-}
-
-func TestPostLoginEmptyUsername(t *testing.T) {
-	test.WithTestServer(t, func(s *api.Server) {
-		payload := test.GenericPayload{
-			"username": "",
-			"password": test.PlainTestUserPassword,
+				test.Snapshoter.Save(t, response)
+			})
 		}
 
-		res := test.PerformRequest(t, s, "POST", "/api/v1/auth/login", payload, nil)
-
-		assert.Equal(t, http.StatusBadRequest, res.Result().StatusCode)
-
-		var response httperrors.HTTPValidationError
-		test.ParseResponseAndValidate(t, res, &response)
-
-		assert.Equal(t, int64(http.StatusBadRequest), *response.Code)
-		assert.Equal(t, httperrors.HTTPErrorTypeGeneric, *response.Type)
-		assert.Equal(t, http.StatusText(http.StatusBadRequest), *response.Title)
-		assert.Empty(t, response.Detail)
-		assert.Nil(t, response.Internal)
-		assert.Nil(t, response.AdditionalData)
-		assert.NotEmpty(t, response.ValidationErrors)
-		assert.Equal(t, "username", *response.ValidationErrors[0].Key)
-		assert.Equal(t, "body", *response.ValidationErrors[0].In)
-		assert.Equal(t, "username in body should be at least 1 chars long", *response.ValidationErrors[0].Error)
-	})
-}
-
-func TestPostLoginEmptyPassword(t *testing.T) {
-	test.WithTestServer(t, func(s *api.Server) {
-		fixtures := test.Fixtures()
-		payload := test.GenericPayload{
-			"username": fixtures.User1.Username,
-			"password": "",
-		}
-
-		res := test.PerformRequest(t, s, "POST", "/api/v1/auth/login", payload, nil)
-
-		assert.Equal(t, http.StatusBadRequest, res.Result().StatusCode)
-
-		var response httperrors.HTTPValidationError
-		test.ParseResponseAndValidate(t, res, &response)
-
-		assert.Equal(t, int64(http.StatusBadRequest), *response.Code)
-		assert.Equal(t, httperrors.HTTPErrorTypeGeneric, *response.Type)
-		assert.Equal(t, http.StatusText(http.StatusBadRequest), *response.Title)
-		assert.Empty(t, response.Detail)
-		assert.Nil(t, response.Internal)
-		assert.Nil(t, response.AdditionalData)
-		assert.NotEmpty(t, response.ValidationErrors)
-		assert.Equal(t, "password", *response.ValidationErrors[0].Key)
-		assert.Equal(t, "body", *response.ValidationErrors[0].In)
-		assert.Equal(t, "password in body should be at least 1 chars long", *response.ValidationErrors[0].Error)
-	})
-}
-
-func TestPostLoginSuccessLowercaseTrimWhitespaces(t *testing.T) {
-	test.WithTestServer(t, func(s *api.Server) {
-		fixtures := test.Fixtures()
-		payload := test.GenericPayload{
-			"username": fmt.Sprintf(" %s ", strings.ToUpper(fixtures.User1.Username.String)),
-			"password": test.PlainTestUserPassword,
-		}
-
-		res := test.PerformRequest(t, s, "POST", "/api/v1/auth/login", payload, nil)
-
-		assert.Equal(t, http.StatusOK, res.Result().StatusCode)
-
-		var response types.PostLoginResponse
-		test.ParseResponseAndValidate(t, res, &response)
-
-		assert.NotEmpty(t, response.AccessToken)
-		assert.NotEqual(t, fixtures.User1AccessToken1.Token, response.AccessToken)
-		assert.NotEmpty(t, response.RefreshToken)
-		assert.NotEqual(t, fixtures.User1RefreshToken1.Token, response.RefreshToken)
-		assert.Equal(t, int64(s.Config.Auth.AccessTokenValidity.Seconds()), *response.ExpiresIn)
-		assert.Equal(t, auth.TokenTypeBearer, *response.TokenType)
 	})
 }
