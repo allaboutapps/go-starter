7f09f25b3ed9d03e596dbd9d6f3f5aaacd675964
Eric Klatzer <eric.klatzer@allaboutapps.at>
2025-05-07 12:40:23 +0000

    Remove unused view templates

--- internal/api/router/templates/templates.go
+++ internal/api/router/templates/templates.go
@@ -4,8 +4,6 @@ type ViewTemplate string
 
 const (
 	ViewTemplateAccountConfirmation ViewTemplate = "account_confirmation.html.tmpl"
-	ViewAppleSiteAssociation        ViewTemplate = "apple-app-site-association.json.tmpl"
-	ViewAndroidAssetlinks           ViewTemplate = "assetlinks.json.tmpl"
 )
 
 func (vt ViewTemplate) String() string {
